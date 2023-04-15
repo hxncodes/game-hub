cf8b44c6122727ba8c3ee66fb7bfdd46b22e1b2f
Hasnat Rana <hxncodes@gmail.com>
2023-04-15 12:04:13 +0500

    Build the color mode Switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
-      <Image src={logo} boxSize="60px" />
-      <Text>hxnCodes</Text>
+    <HStack justifyContent="space-between" padding="10px">
+      <HStack>
+        <Image src={logo} boxSize="60px" />
+        <Text>hxnCodes</Text>
+      </HStack>
+      <ColorModeSwitch />
     </HStack>
   );
 };
