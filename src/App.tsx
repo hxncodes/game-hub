07387b9a657a11155186fb47baf33fa6c4c8344c
Hasnat Rana <hxncodes@gmail.com>
2023-04-15 13:06:33 +0500

    Chakra UI installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button colorScheme="teal">Button</Button>
+    </div>
+  );
 }
 
 export default App;
